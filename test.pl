56c43a963b268101d786d5d02aa6b8510af7a15c
Jeremy Zawodny <Jeremy@Zawodny.com>
2009-03-21 07:40:40 -0700

    into git

---
+++ test.pl
@@ -0,0 +1,19 @@
+##
+## This test framework really doesn't do anything yet.
+##
+
+# Change 1..1 below to 1..last_test_to_print .
+# (It may become useful if the test is moved to ./t subdirectory.)
+
+BEGIN { $| = 1; print "1..1\n"; }
+
+#END {print "not ok 1\n" unless $loaded;}
+
+print "ok 1\n";
+
+######################### End of black magic.
+
+# Insert your test code below (better if it prints "ok 13"
+# (correspondingly "not ok 13") depending on the success of chunk 13
+# of the test code):
+
